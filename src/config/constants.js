5f180c990f6ec51384c5fe300e39d7e468e2e5a9
Jonathan Pe <jonathanqpe@gmail.com>
2024-02-27 18:21:01 -0800

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,13 +1,13 @@
 export default {
-    twitchLink: 'https://twitch.tv/jpeeeeeeeeeeee',
+    twitchLink: 'https://twitch.tv/jpe_TV',
     personalEmail: 'jonathanqpe@gmail.com',
     musicEmail: 'jpemusicofficial@gmail.com',
     calpolyLink: 'https://www.calpoly.edu',
     gliffyLink: 'https://www.gliffy.com',
     stratimLink: 'https://www.stratim.com',
     soundcloudLink: 'https://www.soundcloud.com/jpe-mp3',
-    instagramLink: 'https://instagram.com/jpe.png',
+    instagramLink: 'https://instagram.com/notjpe',
     shows: ['Welcome To The Jungle (04/25/20)', 'Trivecta @ Origin SF (10/18/19)'],
     skills: ['React', 'React Native', 'JavaScript (ES6+)', 'HTML & CSS', 'Node.js', 'Java'],
-    games: ['League of Legends', 'CS:GO', 'Valorant']
+    games: ['League of Legends', 'CS:GO', 'Valorant', 'Apex Legends', 'Path of Exile', 'Last Epoch']
 };
