54ac85eadff5a001de0451a7d156ed084bfcb6f2
Jonathan Pe <jonathanqpe@gmail.com>
2024-02-27 18:20:02 -0800

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,7 +7,7 @@ export default {
     stratimLink: 'https://www.stratim.com',
     soundcloudLink: 'https://www.soundcloud.com/jpe-mp3',
     instagramLink: 'https://instagram.com/jpe.png',
-    shows: ['Welcome To The Jungle, B*TCH (04/25/20)', 'Trivecta @ Origin SF (10/18/19)'],
+    shows: ['Welcome To The Jungle (04/25/20)', 'Trivecta @ Origin SF (10/18/19)'],
     skills: ['React', 'React Native', 'JavaScript (ES6+)', 'HTML & CSS', 'Node.js', 'Java'],
     games: ['League of Legends', 'CS:GO', 'Valorant']
 };
